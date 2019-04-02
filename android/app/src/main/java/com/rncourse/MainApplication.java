c4e160ad874a5843109bdf5849226022d7a1c094
Bhavik Thakkar <thakkarb97@gmail.com>
2019-04-02 12:23:10 +0530

    Revert "Added maps for share place screen"
    This reverts commit 4a437883e4dfe95102f961ded7046c863fcf5504.

--- android/app/src/main/java/com/rncourse/MainApplication.java
+++ android/app/src/main/java/com/rncourse/MainApplication.java
@@ -9,7 +9,6 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,8 +26,7 @@ public class MainApplication extends NavigationApplication {
 		// No need to add RnnPackage and MainReactPackage
 		return Arrays.<ReactPackage>asList(
 			// eg. new VectorIconsPackage()
-      new VectorIconsPackage(),
-      new MapsPackage()
+      new VectorIconsPackage()
 		);
 	}
 
