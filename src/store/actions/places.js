b76648af51bacbaf0cf02544583e5df831fe1f7f
Bhavik Thakkar <thakkarb97@gmail.com>
2019-04-20 22:27:09 +0530

    Stored images on firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -6,15 +6,26 @@ export const addPlace = (placeName, location, image) => {
             name: placeName,
             location: location
         }
-        fetch("https://rn-course-1554131679313.firebaseio.com/places.json", {
+        fetch("https://us-central1-rn-course-1554131679313.cloudfunctions.net/storeImage", {
             method: "POST",
-            body: JSON.stringify(placeData)
+            body: JSON.stringify({
+                image: image.base64
+            })
         })
             .catch(err => console.log(err))
             .then(res => res.json())
             .then(parsedRes => {
                 console.log(parsedRes);
-            });
+            })
+        // fetch("https://rn-course-1554131679313.firebaseio.com/places.json", {
+        //     method: "POST",
+        //     body: JSON.stringify(placeData)
+        // })
+        //     .catch(err => console.log(err))
+        //     .then(res => res.json())
+        //     .then(parsedRes => {
+        //         console.log(parsedRes);
+        //     });
     };
 };
 
