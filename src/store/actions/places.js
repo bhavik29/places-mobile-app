86f9f60ea87401676a3bce7685ec8b1a619576b4
Bhavik Thakkar <thakkarb97@gmail.com>
2019-04-21 17:24:05 +0530

    Store data in firebase database

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,10 +2,6 @@ import * as actionTypes from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        }
         fetch("https://us-central1-rn-course-1554131679313.cloudfunctions.net/storeImage", {
             method: "POST",
             body: JSON.stringify({
@@ -15,17 +11,22 @@ export const addPlace = (placeName, location, image) => {
             .catch(err => console.log(err))
             .then(res => res.json())
             .then(parsedRes => {
-                console.log(parsedRes);
-            })
-        // fetch("https://rn-course-1554131679313.firebaseio.com/places.json", {
-        //     method: "POST",
-        //     body: JSON.stringify(placeData)
-        // })
-        //     .catch(err => console.log(err))
-        //     .then(res => res.json())
-        //     .then(parsedRes => {
-        //         console.log(parsedRes);
-        //     });
+                const placeData = {
+                    name: placeName,
+                    location: location,
+                    image: parsedRes.imageUrl
+                };
+
+                return fetch("https://rn-course-1554131679313.firebaseio.com/places.json", {
+                    method: "POST",
+                    body: JSON.stringify(placeData)
+                })
+                    .catch(err => console.log(err))
+                    .then(res => res.json())
+                    .then(parsedRes => {
+                        console.log(parsedRes);
+                    });
+            });
     };
 };
 
