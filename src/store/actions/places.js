ac92e8371c7d3aa7d585d9a9368685cc6bde59fd
Bhavik Thakkar <thakkarb97@gmail.com>
2019-04-21 17:28:02 +0530

    Commited the conflicted file

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,15 +2,22 @@ import * as actionTypes from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
+<<<<<<< HEAD
         fetch("https://us-central1-rn-course-1554131679313.cloudfunctions.net/storeImage", {
+=======
+        const placeData = {
+            name: placeName,
+            location: location
+        }
+        fetch("https://rn-course-1554131679313.firebaseio.com/places.json", {
+>>>>>>> parent of b76648a... Stored images on firebase
             method: "POST",
-            body: JSON.stringify({
-                image: image.base64
-            })
+            body: JSON.stringify(placeData)
         })
             .catch(err => console.log(err))
             .then(res => res.json())
             .then(parsedRes => {
+<<<<<<< HEAD
                 const placeData = {
                     name: placeName,
                     location: location,
@@ -26,6 +33,9 @@ export const addPlace = (placeName, location, image) => {
                     .then(parsedRes => {
                         console.log(parsedRes);
                     });
+=======
+                console.log(parsedRes);
+>>>>>>> parent of b76648a... Stored images on firebase
             });
     };
 };
